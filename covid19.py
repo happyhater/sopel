7499a70ba3ece5f9713600b7a8f4a48f69a5d65f
happyhater <8759201+happyhater@users.noreply.github.com>
2020-04-08 01:19:56 +0300

    Fixed function country

--- covid19.py
+++ covid19.py
@@ -9,7 +9,7 @@ v0.1 - First version
 v0.2 - Fix lowercase on country
 v0.3 - Added function spam as auto trigger per channel
 v0.4 - Added short name in country: ro
-
+v0.5 - Fixed function country
 
 https://whitehat.ro
 """
@@ -40,7 +40,7 @@ def covid(bot, trigger):
 		country = "romania"
 	try:
 		response = requests.get("https://whitehat.ro/api/covid/?c=" + country.replace(" ", "_").lower(), verify=True)
-		if response.json()["country"].replace(" ", "_").lower() == country.replace(" ", "_").lower(): pass
+		if response.json()["country"]: pass
 	except SSLError:
 		bot.say("SSL Error")
 		return
@@ -49,10 +49,8 @@ def covid(bot, trigger):
 		return
 	if response.json()["country"].lower() == "romania":
 		bot.say("\x02COVID-19\x02 Stats -- \x02\x1F\x0304RO\x0308MAN\x0312IA\x03\x1F\x02 -- Total Cases: \x02" + response.json()['total_cases'] + "\x02, New Cases: \x02\x0302" + response.json()['new_cases'] + "\x03\x02, Total Deaths: \x02" + response.json()['total_deaths'] + "\x02, New Deaths: \x02\x0304" + response.json()['new_deaths'] + "\x03\x02, Recovered: \x02\x0303" + response.json()['total_recovered'] + "\x03\x02, Active Cases: \x02" + response.json()['active_cases'] + "\x02, Critical: \x02" + response.json()['serious_critical'] + "\x02, Total Cases/1M pop: \x02" + response.json()['total_cases_1m_pop'] + "\x02.")
-	elif response.json()["country"].replace(" ", "_").lower() == country.replace(" ", "_").lower():
-		bot.say("\x02COVID-19\x02 Stats -- \x02" + response.json()['country'] +"\x02 -- Total Cases: \x02" + response.json()['total_cases'] + "\x02, New Cases: \x02\x0302" + response.json()['new_cases'] + "\x03\x02, Total Deaths: \x02" + response.json()['total_deaths'] + "\x02, New Deaths: \x02\x0304" + response.json()['new_deaths'] + "\x03\x02, Recovered: \x02\x0303" + response.json()['total_recovered'] + "\x03\x02, Active Cases: \x02" + response.json()['active_cases'] + "\x02, Critical: \x02" + response.json()['serious_critical'] + "\x02, Total Cases/1M pop: \x02" + response.json()['total_cases_1m_pop'] + "\x02.")
 	else:
-		bot.say("We try to help, you try to cheat, guess who won ?")
+		bot.say("\x02COVID-19\x02 Stats -- \x02" + response.json()['country'] +"\x02 -- Total Cases: \x02" + response.json()['total_cases'] + "\x02, New Cases: \x02\x0302" + response.json()['new_cases'] + "\x03\x02, Total Deaths: \x02" + response.json()['total_deaths'] + "\x02, New Deaths: \x02\x0304" + response.json()['new_deaths'] + "\x03\x02, Recovered: \x02\x0303" + response.json()['total_recovered'] + "\x03\x02, Active Cases: \x02" + response.json()['active_cases'] + "\x02, Critical: \x02" + response.json()['serious_critical'] + "\x02, Total Cases/1M pop: \x02" + response.json()['total_cases_1m_pop'] + "\x02.")
 
 @module.interval(3600)
 def spam_every_second(bot):
